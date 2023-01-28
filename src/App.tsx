015edcded2a7710e2e9f84a35df3c2c63ce572b5
MatthewLarsen <matt4.0larsen@gmail.com>
2023-01-27 22:41:53 -0700

    Adding search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react'
 
 interface Quote {
+  _id: string;
   author: string;
   content: string;
 }
 
 function App() {
-  const defaultQuote: Quote = { author: "Matthew Larsen", content: "You'll never see this." };
-  const [randomQuote, setRandomQuote] = useState<Quote | null>(defaultQuote);
+  const [searchPerformed, setSearchPerformed] = useState(false);
+  const [randomQuote, setRandomQuote] = useState<Quote | null>(null);
 
   const [searchText, setSearchText] = useState("");
   const [quotes, setQuotes] = useState<Quote[]>([]);
@@ -22,29 +23,60 @@ function App() {
     loadRandomQuote();
   }, []);
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault(); // Keep the page from refreshing
-    console.log(searchText);
+    setSearchPerformed(true);
+    const result = await fetch("https://usu-quotes-mimic.vercel.app/api/search?query=" + searchText);
+    const quoteList = await result.json();
+    setQuotes(quoteList.results);
   };
 
   return (
     <div className="App">
-      <h2 className="header">Quote Search</h2>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            className="searchBar"
-            type="text"
-            value={searchText}
-            onChange={e => setSearchText(e.target.value)}
-          />
-        </form>
-      </div>
-      { randomQuote != null &&
-      <div>
-        <p className="quote">{ randomQuote.content }</p>
-        <p className="author">-{ randomQuote.author }</p>
-      </div>
+      { !searchPerformed &&
+        <div id="RandomQuote">
+          <h2 className="header">Quote Search</h2>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input
+                className="searchBar"
+                type="text"
+                value={searchText}
+                onChange={e => setSearchText(e.target.value)}
+              />
+            </form>
+          </div>
+          { randomQuote != null &&
+            <div>
+              <p className="quote">{ randomQuote.content }</p>
+              <p className="author">-{ randomQuote.author }</p>
+            </div>
+          }
+        </div>
+      }
+      { searchPerformed &&
+        <div>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input
+                className="searchBar"
+                type="text"
+                value={searchText}
+                onChange={e => setSearchText(e.target.value)}
+              />
+            </form>
+          </div>
+          <div>
+            {
+              quotes.map((quote) => (
+                <div key={quote._id}>
+                  <p className="quote">{ quote.content }</p>
+                  <p className="author">-{ quote.author }</p>
+                </div>
+              ))
+            }
+          </div>
+        </div>
       }
     </div>
   )
